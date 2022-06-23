c324238c66e98786b9092d26271502edfaec4c29
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-06-23 16:43:49 +0000

    hell0 prince

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// HAMBURGER MENU TOGGLER
+const bar = document.querySelector("#nav-bar");
+
+bar.addEventListener("click", function (e) {
+  if (bar.classList.contains("fa-bars")) {
+    bar.classList.remove("fa-bars");
+    bar.classList.add("fa-times");
+  } else {
+    bar.classList.remove("fa-times");
+    bar.classList.add("fa-bars");
+  }
+});
